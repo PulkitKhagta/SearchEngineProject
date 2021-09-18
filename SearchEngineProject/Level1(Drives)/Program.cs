05ec75bc037e56ca9e7eedaa9828a87578f1eab9
PulkitKhagta <khagtapulkit@gmail.com>
2021-09-18 14:25:34 +0530

    Level 1 Completed

--- SearchEngineProject/Level1(Drives)/Program.cs
+++ SearchEngineProject/Level1(Drives)/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace Level1_Drives_
+namespace SearchEngine_ConsoleApp_
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Level 1
+            int c = 0;
+            DriveInfo[] TotalDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo drvinfo in TotalDrives)
+            {
+                Console.WriteLine("Drive{0}", drvinfo.Name);
+                Console.WriteLine("  Drive type: {0}", drvinfo.DriveType);
+                if (drvinfo.IsReady == true)
+                {
+                    Console.WriteLine("Drive Is Active");
+                }
+                else
+                    Console.WriteLine("Drive Is Inactive");
+                c++;
+            }
+            Console.WriteLine("Total no of Drives=" + c);
+            string filename = Console.ReadLine();
+            Console.WriteLine(filename);
+            Console.ReadLine();
         }
     }
 }
